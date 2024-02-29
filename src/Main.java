283032f586cba562e6bae3350baac68a4e4741b8
PhilipMatthewD <128428517+PhilipMatthewD@users.noreply.github.com>
2024-02-29 13:33:00 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-/*public class Main {
+public class Main {
     public static void main(String[] args) {
         Storm monday = new Storm(39.905921, 89, -83.066559, Direction.SOUTH);
         Storm tuesday = new Storm();
         ThunderStorm wednesday = new ThunderStorm(69, 3,420, Direction.NORTH, true);
-        Sharknado thursday = new ThunderStorm(1,1,1, Direction.WEST, true, 10);
+        Sharknado thursday = new Sharknado(1,1,1, Direction.WEST, true, 10);
         System.out.println(Storm.numStorms);
         wednesday.display();
-    }
-}
- */
-public class Main {
-    public static void main(String[] args) {
+        thursday.display();
+        System.out.println("");
+        // ^|Is the Practice we did on tuesday|^
+
+        // v|Is the Practice we did on thursday|v
         Person peter = new Person("Peter Griffin", 100);
         Superhero batman = new Superhero("Bruce Wayne", 300, "Batman");
         System.out.println(peter.toString());
